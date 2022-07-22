ff771f1e989d46976178f265432d101dce7a24b0
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-07-22 17:28:01 +0300

    fix localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ import css from './App.module.css';
 
 export function App() {
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem('contacts')) ?? '';
+    return JSON.parse(localStorage.getItem('contacts')) ?? [];
   });
   const [filter, setFilter] = useState('');
 
